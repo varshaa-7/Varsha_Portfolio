fe99a7493b44ed8f10005b28d59524c923919478
Varsha Awasthi <varsha.awasthi@vwo.com>
2025-06-28 23:30:27 +0530

    cha

--- src/Components/Branding/Copyright.jsx
+++ src/Components/Branding/Copyright.jsx
@@ -2,7 +2,7 @@ const Copyright = () =>{
   return <>
   <p className="text-white opacity-50 text-base">
     <span className="p-1">&copy;</span>
-    2025 Varsha Awasthi.
+    2025 Varsha Awasthi❤️
   </p>
   </>
 }
